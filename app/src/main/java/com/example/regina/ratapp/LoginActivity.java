8764c2c5cf1d13bd2704f1a16c20758ed56f6028
Unknown <rbaker72@gatech.edu>
2017-09-21 15:46:03 -0400

    Added Welcome
    added welcome activity and linked sign in button to log in screen

--- app/src/main/java/com/example/regina/ratapp/LoginActivity.java
+++ app/src/main/java/com/example/regina/ratapp/LoginActivity.java
@@ -127,15 +127,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Callback received when a permissions request has been completed.
      */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+//                                           @NonNull int[] grantResults) {
+//        if (requestCode == REQUEST_READ_CONTACTS) {
+//            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                populateAutoComplete();
+//            }
+//        }
+//    }
 
 
     /**
