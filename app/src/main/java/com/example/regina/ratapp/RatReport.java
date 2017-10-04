76801ae6aa7dcf53344b000aae2a758673fd4f49
Unknown <ebrook3@gatech.edu>
2017-10-04 18:18:49 -0400

    ratreport
    added some variables and an enum class that I haven't filled in.

--- app/src/main/java/com/example/regina/ratapp/RatReport.java
+++ app/src/main/java/com/example/regina/ratapp/RatReport.java
@@ -2,5 +2,13 @@ package com.example.regina.ratapp;
 
 
 public class RatReport {
-    
+    int uniqueKey;
+    String createdData;
+    String locationType; //gonna change to enum
+    int incidentZip;
+    String incidentAddress;
+    String city; //might change to enum
+    String borough; //gonna change to enum
+    double latitude;
+    double longitude;
 }
