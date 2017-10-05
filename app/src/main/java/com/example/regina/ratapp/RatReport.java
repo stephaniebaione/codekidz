3cabf5845e0733d757c8d116d7039e61d70e6662
Unknown <ebrook3@gatech.edu>
2017-10-05 14:40:49 -0400

    RatReport Class
    it has things in it now.

--- app/src/main/java/com/example/regina/ratapp/RatReport.java
+++ app/src/main/java/com/example/regina/ratapp/RatReport.java
@@ -11,4 +11,90 @@ public class RatReport {
     String borough; //gonna change to enum
     double latitude;
     double longitude;
+
+    public RatReport(int uniqueKey, String createdData, String locationType,
+                     int incidentZip, String incidentAddress, String city,
+                     String borough, double latitude, double longitude) {
+        this.uniqueKey = uniqueKey;
+        this.createdData = createdData;
+        this.locationType = locationType;
+        this.incidentZip = incidentZip;
+        this.incidentAddress = incidentAddress;
+        this.city = city;
+        this.borough = borough;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getUniqueKey() {
+        return uniqueKey;
+    }
+
+    public void setUniqueKey(int uniqueKey) {
+        this.uniqueKey = uniqueKey;
+    }
+
+    public String getCreatedData() {
+        return createdData;
+    }
+
+    public void setCreatedData(String createdData) {
+        this.createdData = createdData;
+    }
+
+    public String getLocationType() {
+        return locationType;
+    }
+
+    public void setLocationType(String locationType) {
+        this.locationType = locationType;
+    }
+
+    public int getIncidentZip() {
+        return incidentZip;
+    }
+
+    public void setIncidentZip(int incidentZip) {
+        this.incidentZip = incidentZip;
+    }
+
+    public String getIncidentAddress() {
+        return incidentAddress;
+    }
+
+    public void setIncidentAddress(String incidentAddress) {
+        this.incidentAddress = incidentAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBorough() {
+        return borough;
+    }
+
+    public void setBorough(String borough) {
+        this.borough = borough;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
