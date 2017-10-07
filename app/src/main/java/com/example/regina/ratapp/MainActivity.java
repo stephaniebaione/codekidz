b27ceb83aac4350597bd38292dc851233d853fcb
Unknown <abbyjames4567@gmail.com>
2017-10-07 00:40:18 -0400

    Changed the main activity
    I commented it out but it is still there

--- app/src/main/java/com/example/regina/ratapp/MainActivity.java
+++ app/src/main/java/com/example/regina/ratapp/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.regina.ratapp;
 
+import android.app.LauncherActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ratDataManipulator();
         Button butt = (Button) findViewById(R.id.logOut);
         butt.setOnClickListener(new View.OnClickListener(){ @Override
         public void onClick(View v) {
@@ -23,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
         }});
     }
     public void ratDataManipulator() {
-        DatabaseReference ratData = FirebaseDatabase.getInstance().getReference(
-                "https://dirtyrat-72570.firebaseio.com/");
-        
+        DatabaseReference ratData = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference dataValues = ratData.child("99099");
+        final Button getDataButton = (Button) findViewById(R.id.getData);
+        /*
+        var ratRef = database.ref();
+        ratRef.on('value', function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var childData = childSnapshot.val();
+                childData.
+            });
+        });
+        */
+
+
+
+
     }
 }
