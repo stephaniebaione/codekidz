b588a8f7cdfb2f36a889e42119f9807b888bab83
Unknown <abbyjames4567@gmail.com>
2017-10-14 18:20:28 -0400

    added a method to push rat data to database

--- app/src/main/java/com/example/regina/ratapp/NewSighting.java
+++ app/src/main/java/com/example/regina/ratapp/NewSighting.java
@@ -9,6 +9,12 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class NewSighting extends AppCompatActivity {
 
@@ -99,7 +105,22 @@ public class NewSighting extends AppCompatActivity {
 
         return true;
     }
+    public void pushRatDataToFirebase(RatReport rat) {
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        Map newRatReport = new HashMap();
+        newRatReport.put("Unique Key", rat.getUniqueKey());
+        newRatReport.put("Borough", rat.getBorough());
+        newRatReport.put("City", rat.getCity());
+        newRatReport.put("Created Date", rat.getCreatedData());
+        newRatReport.put("Incident Address", rat.getIncidentAddress());
+        newRatReport.put("Incident Zip", rat.getIncidentZip());
+        newRatReport.put("Latitude", rat.getLatitude());
+        newRatReport.put("Location Type", rat.getLocationType());
+        newRatReport.put("Longitude", rat.getLongitude());
+        database.push().setValue(newRatReport);
 
+
+    }
 }
 
 
