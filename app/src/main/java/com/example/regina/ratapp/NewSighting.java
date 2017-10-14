69c53870d73a37f9ac5f55ac0bf5f0c5a8602044
Unknown <abbyjames4567@gmail.com>
2017-10-14 17:25:05 -0400

    Limits on what can be entered in new report

--- app/src/main/java/com/example/regina/ratapp/NewSighting.java
+++ app/src/main/java/com/example/regina/ratapp/NewSighting.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NewSighting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newreport);
+    }
+
+    public void zipCodeCheck() {
+        EditText zipCodeText = (EditText) findViewById(R.id.editText43);
+        String zipCodeValue = zipCodeText.getText().toString();
+
     }
 }
