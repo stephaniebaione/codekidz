708308d318edf3034ae24a93f2c8b033e57623c2
Stephanie Baione <stephaniebaione@gatech.edu>
2017-10-28 16:06:31 -0400

    populate email skeleton logic added

--- app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
+++ app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
@@ -2,10 +2,15 @@ package com.example.regina.ratapp.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.regina.ratapp.R;
 
 public class UserSettingsActivity extends AppCompatActivity {
+
+    String userEmail = getIntent().getExtras().getString("Email").toString();
+
     /**
      * On create method, create all instances of the objects in whch the user inputs
      * data of the rat report and the button to help the user navigate
@@ -16,5 +21,13 @@ public class UserSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersettings);
 
+        populateEmail();
+    }
+
+    public void populateEmail() {
+//        TextView emailSpace = (TextView) findViewById(R.id.EmailBlank);
+//        emailSpace.setText(userEmail, TextView.BufferType.EDITABLE);
+
+        //we need to load the email from the user into the EmailBlank
     }
 }
