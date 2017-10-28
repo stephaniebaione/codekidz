2550c26f4dec2a3d4b84b7a5eaa6a75d78375c55
Unknown <abbyjames4567@gmail.com>
2017-10-28 15:48:52 -0400

    Cleaned up query Manager

--- app/src/main/java/com/example/regina/ratapp/Model/QueryManager.java
+++ app/src/main/java/com/example/regina/ratapp/Model/QueryManager.java
@@ -7,6 +7,7 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
@@ -28,6 +29,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Timer;
 
 /**
  * Created by Abby on 10/26/2017.
@@ -42,77 +44,6 @@ public class QueryManager {
         this.activity = activity;
     }
 
-    public Activity getActivity() {
-        return activity;
-    }
-    /**
-     * Sorts Data and saves the data that falls within the selected date range
-     * @param firstMonth the earliest month chosen for viewing
-     * @param lastMonth the latest month chosen for viewing
-     * @param firstYear the first year the user wants to view
-     * @param lastYear the same year or next year the user wants to view
-     * @return
-     */
-
-    public HashMap<Integer, RatReport> getDateDataList(final String firstMonth, final String lastMonth,
-                                                       final String firstYear, final String lastYear) {
-        Log.d("Testing", "get here");
-        DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference().getRoot();
-        firebaseDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.d("Testing", "run");
-                int firstMonthInt = Integer.parseInt(firstMonth);
-                int lastMonthInt = Integer.parseInt(lastMonth);
-                int firstYearInt = Integer.parseInt(firstYear);
-                int lastYearInt = Integer.parseInt(lastYear);
-                for (DataSnapshot ratData: dataSnapshot.getChildren()) {
-                    String date = ratData.child("Created Date").getValue().toString();
-                    Log.d("BAAAAAAA", "meh" + ratData.getChildrenCount());
-                    String[] parts = date.split("/");
-                    int month = Integer.parseInt(parts[0]);
-                    int year = Integer.parseInt(parts[parts.length - 1].substring(0,4));
-                    //Checks if dates chosen are through the same year
-                    if (year == firstYearInt && firstYearInt == lastYearInt) {
-                        // Checks if dates are between the two chosen months
-                        if(month >= firstMonthInt
-                                && month <= lastMonthInt) {
-                            RatReport ratReport = createReport(ratData);
-                           addtoRightDateList(ratReport);
-                            Log.d("whhhhyyyyy", "newval" + " " + rightDateList.size());
-
-                        }
-                        // Check statement for a span of more than one year
-                    } else {
-                        if ((year == firstYearInt && month >= firstMonthInt) || (year > firstYearInt
-                                && year < lastYearInt) || (year == lastYearInt
-                                && month <= lastMonthInt)) {
-                            RatReport ratReport = createReport(ratData);
-                            addtoRightDateList(ratReport);
-                            Log.d("whhhhyyyyy", "newval" + " " + rightDateList.size());
-                        }
-                    }
-                }
-                Log.d("whhhhyyyyy", "final" + " " + rightDateList.size());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-        Log.d("YOOOOOOOOO", "check" + " " + rightDateList.size());
-        return getRightDateList();
-
-
-    }
-    public void addtoRightDateList(RatReport report) {
-        rightDateList.put(report.getUniqueKey(), report);
-    }
-
-    public HashMap<Integer, RatReport> getRightDateList() {
-        return rightDateList;
-    }
 
     /**
      * Helper Function that makes a rat Report from given firebase data
@@ -153,9 +84,15 @@ public class QueryManager {
         return new DateSearcher();
     }
 
+
     public class DateSearcher extends AsyncTask<String, Void, Integer> {
         Dialog dialog = new ProgressDialog(activity);
 
+        /**
+         *
+         * @param args takes in the first month,last month, first year, last month
+         * @return a dummy int to move on
+         */
         @Override
         public Integer doInBackground(String...args) {
             Query firebaseDatabase = FirebaseDatabase.getInstance().getReference().getRoot();
@@ -220,98 +157,15 @@ public class QueryManager {
             return 7;
 
         }
-
+        // creates a dialog box to show the user it is processing
         protected void onPreExecute() {
             dialog.setTitle("Loading");
             dialog.setCancelable(false);
             dialog.show();
+
         }
+        //closes dialog when done
         protected void onPostExecute(Integer searched) {
-            //activity.addMarkers(searched);
-            dialog.dismiss();
-            Log.d("Debug", "executed");
-
-        }
-
-
-    }
-    //*****************************************************************************************
-    public class DateSearcher2 extends AsyncTask<Void, Void, Integer> {
-        Dialog dialog = new ProgressDialog(activity);
-        DateSearcher2(String firstMonth,String firstYear, String lastMonth, String lastYear) {
-            this.firstMonth = firstMonth;
-            this.firstYear = firstYear;
-            this.lastMonth = lastMonth;
-            this.lastYear = lastYear;
-        }
-        final String firstMonth;
-        final String firstYear;
-        final String lastMonth;
-        final String lastYear;
-        @Override
-        public Integer doInBackground(Void...params) {
-
-            return 7;
-
-        }
-
-        protected void onPreExecute() {
-            dialog.setTitle("Loading");
-            dialog.setCancelable(false);
-            dialog.show();
-            Query firebaseDatabase = FirebaseDatabase.getInstance().getReference().getRoot();
-            final HashMap<Integer, RatReport> rightDateList = new HashMap<>();
-
-            firebaseDatabase.addListenerForSingleValueEvent (new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    Log.d("Debug","listened");
-
-                    int firstMonthInt = Integer.parseInt(firstMonth);
-                    int lastMonthInt = Integer.parseInt(lastMonth);
-                    int firstYearInt = Integer.parseInt(firstYear);
-                    int lastYearInt = Integer.parseInt(lastYear);
-                    //int count = 2;
-                    for (DataSnapshot ratData: dataSnapshot.getChildren()) {
-                        String date = ratData.child("Created Date").getValue().toString();
-                        String[] parts = date.split("/");
-                        int month = Integer.parseInt(parts[0]);
-                        int year = Integer.parseInt(parts[2].substring(0,4));
-                        //Checks if dates chosen are through the same year
-                        if (year == firstYearInt && firstYearInt == lastYearInt) {
-                            // Checks if dates are between the two chosen months
-                            if(month >= firstMonthInt
-                                    && month <= lastMonthInt) {
-                                RatReport ratReport = createReport(ratData);
-                                LatLng sydney = new LatLng(ratReport.getLatitude(), ratReport.getLongitude());
-                                //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                                rightDateList.put(ratReport.getUniqueKey(), ratReport);
-                                activity.addMarkers(ratReport);
-                                Log.d("hhhhhhhhhhhhhhh", "being added" + rightDateList.size());
-                            }
-                            // Check statement for a span of more than one year
-                        } else {
-                            if ((year == firstYearInt && month >= firstMonthInt) || (year > firstYearInt
-                                    && year < lastYearInt) || (year == lastYearInt
-                                    && month <= lastMonthInt)) {
-                                RatReport ratReport = createReport(ratData);
-                                rightDateList.put(ratReport.getUniqueKey(), ratReport);
-                                activity.addMarkers(ratReport);
-                            }
-                        }
-                    }
-
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    Log.d("Debug", "Canceled");
-                }
-            });
-            Log.d("hhh22222", "second" + rightDateList.size());
-        }
-        protected void onPostExecute(Integer searched) {
-            //activity.addMarkers(searched);
             dialog.dismiss();
             Log.d("Debug", "executed");
 
