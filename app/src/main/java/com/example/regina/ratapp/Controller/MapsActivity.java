f2996352603063ec8c7bb14355154dd78ae3bfbb
Unknown <abbyjames4567@gmail.com>
2017-10-28 15:21:00 -0400

    Push updated sorting stuff

--- app/src/main/java/com/example/regina/ratapp/Controller/MapsActivity.java
+++ app/src/main/java/com/example/regina/ratapp/Controller/MapsActivity.java
@@ -69,7 +69,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
     public void addMarkers(RatReport addList) {
        // for (RatReport value: addList.values()) {
