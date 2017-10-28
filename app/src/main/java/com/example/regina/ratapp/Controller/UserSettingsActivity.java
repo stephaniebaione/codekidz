aa1ecc23f375a94b8154b5f949204eb29dfe4fca
Unknown <rbaker72@gatech.edu>
2017-10-28 17:03:12 -0400

    Email is showing up in settings
    the user's email is now showing up in the settings page

--- app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
+++ app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
@@ -2,10 +2,14 @@ package com.example.regina.ratapp.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.regina.ratapp.R;
 
 public class UserSettingsActivity extends AppCompatActivity {
+
+    TextView uEmail;
     /**
      * On create method, create all instances of the objects in whch the user inputs
      * data of the rat report and the button to help the user navigate
@@ -15,6 +19,8 @@ public class UserSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersettings);
+        String em = getIntent().getExtras().getString("Email");
+        ((TextView) findViewById(R.id.EmailBlank)).setText(em);
 
     }
 }
