415e2069a1236262fc36801065f083e727dcd196
Stephanie Baione <stephaniebaione@gatech.edu>
2017-11-01 00:40:10 -0400

    cleaned userSettings

--- app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
+++ app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
@@ -2,15 +2,11 @@ package com.example.regina.ratapp.Controller;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.regina.ratapp.R;
 
 public class UserSettingsActivity extends AppCompatActivity {
-
-    String userEmail = getIntent().getExtras().getString("Email").toString();
-    TextView uEmail;
     /**
      * On create method, create all instances of the objects in whch the user inputs
      * data of the rat report and the button to help the user navigate
@@ -20,16 +16,13 @@ public class UserSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersettings);
-        String em = getIntent().getExtras().getString("Email");
-        ((TextView) findViewById(R.id.EmailBlank)).setText(em);
+
 
         populateEmail();
     }
 
     public void populateEmail() {
-//        TextView emailSpace = (TextView) findViewById(R.id.EmailBlank);
-//        emailSpace.setText(userEmail, TextView.BufferType.EDITABLE);
-
-        //we need to load the email from the user into the EmailBlank
+        String em = getIntent().getExtras().getString("Email");
+        ((TextView) findViewById(R.id.EmailBlank)).setText(em);
     }
 }
