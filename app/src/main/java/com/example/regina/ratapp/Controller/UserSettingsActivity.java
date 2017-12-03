e3952c59a77ea3aa7efb5ec94ffa31003917c9a6
Unknown <rbaker72@gatech.edu>
2017-12-03 11:12:43 -0500

    added password reset and rat photo
     pssword reset functionality is implemented in user settings and Peter Beans is on the main screen

--- app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
+++ app/src/main/java/com/example/regina/ratapp/Controller/UserSettingsActivity.java
@@ -1,12 +1,24 @@
 package com.example.regina.ratapp.Controller;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.regina.ratapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserSettingsActivity extends AppCompatActivity {
+    private Button changePasswordBtn;
+    private Button changeEmailBtn;
+    private FirebaseAuth mAuth;
+    private String uEmail;
     /**
      * On create method, create all instances of the objects in which the user inputs
      * data of the rat report and the button to help the user navigate
@@ -16,16 +28,45 @@ public class UserSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usersettings);
-
-
         populateEmail();
+
+        changePasswordBtn = (Button) findViewById(R.id.ChangePasswordButton);
+        changePasswordBtn = (Button) findViewById(R.id.ChangeEmailButton);
+
+        mAuth = FirebaseAuth.getInstance();
+        changePasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.sendPasswordResetEmail(uEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            AlertDialog.Builder dialog = new AlertDialog.Builder(UserSettingsActivity.this);
+                            dialog.setCancelable(true);
+                            dialog.setTitle("Reset Password");
+                            dialog.setMessage("An email has been sent. Check your email to change.");
+                            dialog.setPositiveButton("Ok", null);
+                            dialog.show();
+                        } else {
+                            AlertDialog.Builder dialog = new AlertDialog.Builder(UserSettingsActivity.this);
+                            dialog.setCancelable(true);
+                            dialog.setTitle("Reset Password");
+                            dialog.setMessage("Email failed to send. Try again.");
+                            dialog.setPositiveButton("Ok", null);
+                            dialog.show();
+                        }
+                    }
+                });
+            }
+        });
+        
     }
 
     /**
      * sets the email textView to have the current user's email
      */
     private void populateEmail() {
-        String em = getIntent().getExtras().getString("Email");
-        ((TextView) findViewById(R.id.EmailBlank)).setText(em);
+        uEmail = getIntent().getExtras().getString("Email");
+        ((TextView) findViewById(R.id.EmailBlank)).setText(uEmail);
     }
 }
